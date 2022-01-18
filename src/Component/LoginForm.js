644be9350414a0d17497ef16b134c10a0485cf2b
masbagasgpn <masbagaspn@gmail.com>
2022-01-18 20:08:56 +0700

    update handle login-form

--- src/Component/LoginForm.js
+++ src/Component/LoginForm.js
@@ -2,51 +2,87 @@ import React from 'react';
 import './Form.css';
 import logo from './Asset/logo.png';
 
-function LoginForm() {
-    return (
-        <div className='content-container'>
-            <img src={logo} alt="" className='logo'/>
-            <div className='text-container'>
-                <h2>Hello!</h2>
-                <p>Welcome! Please fill in with your details.</p>
-            </div>
-            <form action="https://super-auto-cash-mobile-api.herokuapp.com" className='form'>
-                <div className='form-group'>
-                    <label htmlFor="username" className='form-label'>Username<span className='required'>*</span></label>
-                    <input type="text" 
-                        id="username" 
-                        className="form-control" 
-                        minLength={8}
-                        aria-describedby='username-control'
-                        placeholder='Enter your username'
-                        autoComplete="off"
-                        required/>
-                    <span id="username-control" 
-                        aria-live="assertive" 
-                        className="validation-message">
-                    </span>
-                    <label htmlFor="password" className='form-label'>Password<span className='required'>*</span></label>
-                    <input type="text" 
-                        id="password" 
-                        className="form-control" 
-                        minLength={8}
-                        aria-describedby='password-control'
-                        placeholder='Enter your password'
-                        autoComplete="off"
-                        required/>
-                    <span id="password-control" 
-                        aria-live="assertive" 
-                        className="validation-message">
-                    </span>
+class LoginForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            input: {
+                username: '',
+                password: ''
+            },
+            submit: ''
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+      }
+      handleUsername(event) {
+        this.setState({
+          input: {
+              username: event.target.value
+          }
+        });
+      }
+      handlePassword(event) {
+        this.setState({
+          input: {
+              password: event.target.value
+          }
+        });
+      }
+      handleSubmit(event) {
+        event.preventDefault()
+        this.setState({
+          submit: this.state.input
+        });
+      }
+    render() {
+        return (
+            <div className='content-container'>
+                <img src={logo} alt="" className='logo'/>
+                <div className='text-container'>
+                    <h2>Hello!</h2>
+                    <p>Welcome! Please fill in with your details.</p>
                 </div>
-                <button className='btn-submit'
-                    type='submit'>
-                    Sign in
-                </button>
-                <span className='anchor-form'>Don't have an account yet?&nbsp;<a href="#blank">Login here!</a></span>
-            </form>
-        </div>
-    )
+                <form action="https://super-auto-cash-mobile-api.herokuapp.com" className='form'>
+                    <div className='form-group'>
+                        <label htmlFor="username" className='form-label'>Username<span className='required'>*</span></label>
+                        <input type="text" 
+                            id="username" 
+                            className="form-control" 
+                            minLength={8}
+                            aria-describedby='username-control'
+                            placeholder='Enter your username'
+                            autoComplete="off"
+                            onChange={handleUsername}
+                            required/>
+                        <span id="username-control" 
+                            aria-live="assertive" 
+                            className="validation-message">
+                        </span>
+                        <label htmlFor="password" className='form-label'>Password<span className='required'>*</span></label>
+                        <input type="text" 
+                            id="password" 
+                            className="form-control" 
+                            minLength={8}
+                            aria-describedby='password-control'
+                            placeholder='Enter your password'
+                            autoComplete="off"
+                            onChange={handleUsername}
+                            required/>
+                        <span id="password-control" 
+                            aria-live="assertive" 
+                            className="validation-message">
+                        </span>
+                    </div>
+                    <button className='btn-submit'
+                        type='submit'>
+                        Sign in
+                    </button>
+                    <span className='anchor-form'>Don't have an account yet?&nbsp;<a href="#blank">Login here!</a></span>
+                </form>
+            </div>
+        )
+    } 
 }
 
 export default LoginForm;
